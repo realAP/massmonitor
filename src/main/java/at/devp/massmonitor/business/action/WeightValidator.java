970abdea5633f8555ee232a299253497a184cbc5
Axel Schwab <Axel.Schwab94@gmail.com>
2023-08-11 11:02:38 +0200

    wrong import of service

--- src/main/java/at/devp/massmonitor/business/action/WeightValidator.java
+++ src/main/java/at/devp/massmonitor/business/action/WeightValidator.java
@@ -1,13 +1,13 @@
 package at.devp.massmonitor.business.action;
 
-import org.jvnet.hk2.annotations.Service;
+
+import org.springframework.stereotype.Service;
 
 import javax.xml.bind.ValidationException;
 import java.util.regex.Pattern;
 
 @Service
 public class WeightValidator {
-
   private final Pattern WEIGHT_PATTERN = Pattern.compile(
       "^(\\d{1,3})(,\\d{1,2})?$");
 
